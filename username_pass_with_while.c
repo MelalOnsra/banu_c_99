56ba47ea788d3845ecdf65498267ec81977e77ab
Berkant <119416073+MelalOnsra@users.noreply.github.com>
2022-11-29 14:52:22 +0300

    Update username_pass_with_while.c

--- username_pass_with_while.c
+++ username_pass_with_while.c
@@ -1,1 +1,20 @@
-// cretae a username_pass_with_while. 
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "ctype.h"
+
+void login(char username[], char password[]) {
+
+  
+}
+
+int main() {
+    char username[20];
+    char password[20];
+    printf("Enter your username: ");
+    scanf("%s", username);
+    printf("Enter your password: ");
+    scanf("%s", password);
+    login(username, password);
+    return 0;
+}
